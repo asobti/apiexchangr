fb1c389dc748406bebc83e2c92826fb2dfeaf161
Ayush <ayushsobti@gmail.com>
2012-10-08 23:34:10 -0400

    get and post methods

--- application/controllers/exchangr.php
+++ application/controllers/exchangr.php
@@ -4,33 +4,51 @@ class Exchangr extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		$this->load->library('curl');
+		$this->load->library('curl');		
 	}
 
     public function index()
     {
+    	//print_r($_GET); die();
 		// make the API call using the same Request method the user used
 		$endpoint = $this->input->get_post('endpoint');
 
 		if ($endpoint !== FALSE) {
 
 			$endpoint = urldecode($endpoint);
+			$response = '';
 
-			$post_payload = array();			
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				$response = $this->post($endpoint);
+			} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+				$response = $this->get($endpoint);
+			}
 
-			foreach($_POST as $key=>$value) {
-				if ($key == 'endpoint') {
-					continue;
-				}
-
-				$post_payload[$key] = $value;
-			}			
+			print_r($response);
 
 		} else {
 			// change this to an appropriate reply
 			// with correct headers. 
 			die('No endpoint specified');
 		}
+    }
+
+    private function post($endpoint) {
+    	$payload = array();
+
+    	foreach($_POST as $key=>$value) {
+    		if ($key == 'endpoint') {
+    			continue;
+    		}
+
+    		$payload[$key] = $value;
+    	}
+
+    	return $this->curl->simple_post($endpoint, $payload);
+    }
+
+    private function get($endpoint) {
+    	return $this->curl->simple_get('$endpoint');
     }
 
 }
