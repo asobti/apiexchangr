02f5814538eaff616a45a0835fc763196831e566
Ayush <ayushsobti@gmail.com>
2012-10-08 22:49:58 -0400

    set config options in CI

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "exchangr";
 $route['404_override'] = '';
 
 
---
+++ application/controllers/exchangr.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Exchangr extends CI_Controller {
+
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file exchangr.php */
+/* Location: ./application/controllers/exchangr.php */
