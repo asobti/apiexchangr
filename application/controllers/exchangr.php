e6bc9d3f0846651c509996cb2afcf5eab70268f3
Ayush <ayushsobti@gmail.com>
2012-10-08 23:11:28 -0400

    stubbed out index method

--- application/controllers/exchangr.php
+++ application/controllers/exchangr.php
@@ -2,9 +2,35 @@
 
 class Exchangr extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->load->library('curl');
+	}
+
     public function index()
     {
-        
+		// make the API call using the same Request method the user used
+		$endpoint = $this->input->get_post('endpoint');
+
+		if ($endpoint !== FALSE) {
+
+			$endpoint = urldecode($endpoint);
+
+			$post_payload = array();			
+
+			foreach($_POST as $key=>$value) {
+				if ($key == 'endpoint') {
+					continue;
+				}
+
+				$post_payload[$key] = $value;
+			}			
+
+		} else {
+			// change this to an appropriate reply
+			// with correct headers. 
+			die('No endpoint specified');
+		}
     }
 
 }
